39e2cdf1ebe6319964ee03c25977489cc9ff067a
Samar Panda <samar.panda84@gmail.com>
2015-03-09 19:13:42 +0530

    Travis CI added

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var assert = require('assert');
 gSearchTrends.result(function(err, res){
 	if(!err){
 		assert.deepEqual(Object.keys(res).length, 47, "Must have 47 countries top search trends");
+		assert.equal(Object.keys(res).length, 47, "Result consists of 47 countries top search trends");
 	} else {
 		assert.ok(err);
 	}
