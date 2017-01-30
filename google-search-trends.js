428e24e4a53310137714310c6ff60cc66f367b96
Samar Panda <er.samarpanda@gmail.com>
2017-01-30 21:23:28 +0530

    Adding new method Trends by country

--- google-search-trends.js
+++ google-search-trends.js
@@ -1,5 +1,7 @@
 var request = require('request');
 var googleTrendUrl = "http://hawttrends.appspot.com/api/terms/";
+var trendsByCountryUrl = "https://www.google.com/trends/hottrends/visualize/internal/data";
+const ALL_REGIONS = "all_regions";
 
 var result = function(cb){
 	request(googleTrendUrl, function(error, response, body){
@@ -11,4 +13,22 @@ var result = function(cb){
 	});
 }
 
+var trendsByCountry = function(country = ALL_REGIONS, cb){
+  request(trendsByCountryUrl, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      let result = JSON.parse(body)
+      if(country === ALL_REGIONS){
+        return cb(null, result)
+      }else if(result.hasOwnProperty(country)){
+        return cb(null, result[country])
+      }else{
+        return cb(new Error('Sorry trends not avaliable for '+country), null)
+      }
+    }else{
+      return cb(new Error(error), null);
+    }
+  });
+}
+
 exports.result = result;
+exports.trendsByCountry = trendsByCountry;
